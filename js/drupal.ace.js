805edf69118ac39cb5490d57b0822a6017a77093
David Flores <dmousex@gmail.com>
2015-01-11 23:02:31 -0600

    Define the base configuration

--- js/drupal.ace.js
+++ js/drupal.ace.js
@@ -1,5 +1,32 @@
+(function (Drupal, debounce, $, ace) {
+  "use strict";
 
-var editor = ace.edit("editor");
-var edit_session = editor.setTheme("ace/theme/monokai")
-edit_session.setTabSize(2)
-editor.getSession().setMode("ace/mode/html");
+  Drupal.editors.ace_editor = {
+
+    attach: function (element, format) {
+      var textarea = $("#edit-body-0-value");
+      var mode = textarea.data('editor');
+
+      var editDiv = $('<div>', {
+          position: 'absolute',
+          width: textarea.width(),
+          height: textarea.height(),
+          'class': textarea.attr('class')
+      }).insertBefore(textarea);
+
+      textarea.css('visibility', 'hidden');
+      var aceEditor = ace.edit(editDiv[0]);
+      aceEditor.setTheme("ace/theme/monokai");
+      aceEditor.getSession().setMode("ace/mode/markdown");
+      editor.setOption("enableEmmet", true);
+    },
+
+    detach: function (element, format, trigger) {},
+
+    onChange: function (element, callback) {},
+
+    attachInlineEditor: function (element, format, mainToolbarId, floatedToolbarId) {},
+
+  };
+
+})(Drupal, Drupal.debounce, jQuery, ace);
